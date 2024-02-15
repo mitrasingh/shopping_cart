49ab462aa542405185b1e1b3850277d65ff9be07
Mitra Singh <mitra.coder@gmail.com>
2024-02-14 20:27:54 -0500

    import components for react router and react-bootstrap, seting up structure of app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
 function App() {
-	return <div></div>;
+	return (
+		<Container>
+			<Routes>
+				<Route />
+			</Routes>
+		</Container>
+	);
 }
 
 export default App;

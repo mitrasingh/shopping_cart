2f721d89d6b31d0feb243df059789a74c95485ff
Mitra Singh <mitra.coder@gmail.com>
2024-02-17 16:19:08 -0500

    updated file name

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,9 @@
+import { Navbar, Container } from "react-bootstrap";
+
+export const Navigation = () => {
+	return (
+		<Navbar className="bg-white shadow-sm mb-3">
+			<Container>Nav</Container>
+		</Navbar>
+	);
+};

d2cacc07e4f9383699eea7dd0f70563182248e56
Mitra Singh <mitra.coder@gmail.com>
2024-02-17 16:33:27 -0500

    update navigation import line

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Container } from "react-bootstrap";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
-import { Navbar } from "./components/Navbar";
+import { Navigation } from "./components/Navigation";
 
 function App() {
 	return (
 		<>
-			<Navbar />
+			<Navigation />
 			<Container className="mb-4">
 				<Routes>
 					<Route path="/" element={<Home />} />

9a25d37c483a1abda8f506d63c38c91f3d11800a
Mitra Singh <mitra.coder@gmail.com>
2024-02-19 10:31:58 -0500

    added storeitem component which will map through product items

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,5 @@
 import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
 import storeItems from "../data/items.json";
 
 export const Store = () => {
@@ -7,7 +8,9 @@ export const Store = () => {
 			<h1>Store</h1>
 			<Row md={2} lg={3} className="g-3">
 				{storeItems.map((item) => (
-					<Col>{JSON.stringify(item)}</Col>
+					<Col key={item.id}>
+						<StoreItem {...item} />
+					</Col>
 				))}
 				<Col></Col>
 			</Row>
